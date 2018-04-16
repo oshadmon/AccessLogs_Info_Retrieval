11700845f656a78101d1812d3678ef38c1c2c239
Ori Shadmon <oshadmon@gmail.com>
2018-04-16 15:42:28 -0700

    Added api-key, query, and radius requirments

--- generate_info.py
+++ generate_info.py
@@ -39,8 +39,11 @@ class GenerateInfo:
          self.org:str        - Organization that repo falls under 
          self.repo:str       - Git Repository (if relevent) 
          
-         # Non Git config 
+         # None Git config 
          self.file_name:str  - file containing source info  (for non-Git sources)
+         self.api_key:str    - Google API Key
+         self.query:str      - Category which location falls under (such as lunch)
+         self.radius:int     - How far from origin to find locations
          self.downloads:bool - only store download results 
          self.traffic:bool   - only store traffic results   
          
@@ -56,6 +59,9 @@ class GenerateInfo:
       self.org='NewOrg' # GitHub repository (if set to 'NewOrg' then uses GitHub user instead)  
       self.repo='NewRepo' # Git Repository 
       self.file_name='/tmp/output.txt' # For none Git sources  
+      self.api_key='AAABBBCCCDDD1234'
+      self.query='lunch' 
+      self.radius=0 
       self.downloads=True
       self.traffic=True
       self.psql=False
@@ -63,7 +69,7 @@ class GenerateInfo:
       for value in values: 
          if value is sys.argv[0]: 
             pass 
-         if "--host" in value.lower(): 
+         elif "--host" in value.lower(): 
             self.host = str(value.split("=")[-1].split(":")[0])
             self.port = int(value.split("=")[-1].split(":")[1])
          elif "--user" in value.lower(): 
@@ -87,6 +93,12 @@ class GenerateInfo:
             self.downloads = False 
          elif "--psql" in value.lower(): 
             self.psql = True
+         elif "--api-key" in value.lower(): 
+            self.api_key = str(value.split("=")[-1])
+         elif "--query" in value.lower(): 
+            self.query = str(value.split("=")[-1])
+         elif "--radius" in value.lower(): 
+            self.radius = int(value.split("=")[-1]) 
          else: 
             self._help(value=value) 
          # If user doesn't specify org, then use authentication username 
@@ -108,11 +120,14 @@ class GenerateInfo:
            +"\n\t--source: Where the data is coming from  [--source=NewSource]" 
            +"\n\t--psql: use PostgresSQL instead of MySQL " 
            +"\n\nGitHub Requirements" 
-           +"\n\t--git-auth: GitHub authentication information                                 [--git-auth=user@githbu.com:password]"
+           +"\n\t--git-auth: GitHub authentication information                                 [--git-auth='user@githbu.com':'password']"
            +"\n\t--org: Organization that repo falls under (if none then uses GitHub username) [--org=NewOrg]" 
            +"\n\t--repo: GitHub Repository Name                                                [--repo=NewRepo]"
            +"\n\nFile based insight" 
            +"\n\t--fine-name: file name and path with insight                                  [--file-name=$HOME/tmp.txt]" 
+           +"\n\t--api-key: Google's API Key                                                   [--api-key=AAABBBCCC-123]" 
+           +"\n\t--query: Category location falls under                                        [--query='lunch spot']"
+           +"\n\t--radius: Distance from origin                                                [--radius=0]" 
            +"\n\t--downloads: Write only to downloads table (false by default)"  
            +"\n\t--traffic: Write only to traffic table (false by default)"
            ) 
@@ -120,7 +135,7 @@ class GenerateInfo:
 
    def main(self): 
       if self.source.lower() in ['aws', 'website']: 
-         gii = GenerateIPBasedInfo(cur=self.c, file_name=self.file_name, source=self.source) 
+         gii = GenerateIPBasedInfo(cur=self.c, file_name=self.file_name, source=self.source, api=self.api_key, query=self.query, radius=self.radius) 
          if self.downloads is True: 
             gii.download_ip() 
          if self.traffic is True: 

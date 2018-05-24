e0c9016e3c7d37d8a0502edabbe7beaeae6aa1a6
Ori Shadmon <oshadmon@gmail.com>
2018-05-24 11:33:18 -0700

    new create table statements

--- extras/create_table.sql
+++ extras/create_table.sql
@@ -1,52 +1,14 @@
-DROP TABLE IF EXISTS ip_data; 
-DROP TABLE IF EXISTS github_data; 
-DROP TABLE IF EXISTS github_referral_list; 
-DROP TABLE IF EXISTS traffic; 
-DROP TABLE IF EXISTS downloads; 
+DROP TABLE IF EXISTS raw_ip_data; 
+DROP TABLE IF EXISTS ip_stats; 
 
--- Table with data regarding IP address (frequency, location, potential places) 
-CREATE TABLE ip_data(
-   id SERIAL, 
-   create_timestamp TIMESTAMP DEFAULT now(), 
-   update_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-   ip VARCHAR(255) NOT NULL DEFAULT '127.0.0.1', 
-   source VARCHAR(255) NOT NULL DEFAULT 'AWS', 
-   total_access INT NOT NULL DEFAULT 0, -- Total Number of accessed  
-   access_times TEXT, -- AWS Accessed Timestamps 
-   coordiantes VARCHAR(255), -- Coordinates 
-   address VARCHAR(255) NOT NULL DEFAULT '', -- Address
-   places TEXT, -- Places
-   PRIMARY KEY (id)
-); 
-CREATE INDEX source ON ip_data(source, ip); 
-
--- Information regarding GitHub referrals 
-CREATE TABLE github_referral_list(
-   id SERIAL,
-   create_timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
-   repo VARCHAR(255) NOT NULL DEFAULT 'NewRepo',  
-   referral VARCHAR(255) NOT NULL DEFAULT '', 
-   daily_referrals INT NOT NULL DEFAULT 0, 
-   PRIMARY KEY(id)
+CREATE TABLE raw_ip_data(
+   ip VARCHAR(255) NOT NULL DEFAULT '127.0.0.1',
+   access_timestamp TIMESTAMP DEFAULT now()
 ); 
 
--- Information Downloads
-CREATE TABLE downloads(
-   id SERIAL,
-   create_timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
-   source VARCHAR(255) NOT NULL DEFAULT 'GitHub', 
-   repo VARCHAR(255) NOT NULL DEFAULT 'NewRepo',
-   daily_download INT NOT NULL DEFAULT 0,
-   PRIMARY KEY(id)
+CREATE TABLE ip_stats(
+   ip VARCHAR(255) NOT NULL DEFAULT '127.0.0.1',
+   frequency INT DEFAULT 1
 );
 
--- Information regarding Traffic 
-CREATE TABLE traffic(
-   id SERIAL,
-   create_timestamp TIMESTAMP NOT NULL DEFAULT NOW(),
-   source VARCHAR(255) NOT NULL DEFAULT 'GitHub',
-   repo VARCHAR(255) NOT NULL DEFAULT 'NewRepo',
-   daily_traffic INT NOT NULL DEFAULT 0,
-   PRIMARY KEY(id)
-);
 
